15f24432c24cafbb44fa85ef1437ed19ba807823
Srajan Gowda <srajangowda95@gmail.com>
2025-03-12 22:03:28 +0530

    Create loops.js

---
+++ loops.js
@@ -0,0 +1,15 @@
+let a=10;
+ document.getElementById("phara").innerHTML=a;
+
+ let x;
+ for(i=0;i<5;i++){
+    console.log(i);
+
+ }
+  let x=1;
+ while(x<=5){
+    console.log(x);
+    x++;
+   }
+ document.getElementById("phara").innerHTML=x;
+ 
